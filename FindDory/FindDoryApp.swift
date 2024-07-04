c98208d13bd90828229dea35d4128d37f8e56b50
Lucasanthiago <santoslblucas@gmail.com>
2024-07-04 15:43:27 -0300

    Initial Commit

---
+++ FindDory/FindDoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindDoryApp.swift
+//  FindDory
+//
+//  Created by Lucas Santos on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FindDoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
